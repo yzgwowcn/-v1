0c9b4cd88fd8f473fa1d88f708067b88148cba1a
yzgwowcn <yzgwowcn@gmail.com>
2025-12-23 21:52:29 +0800

    添加链接

--- constants.ts
+++ constants.ts
@@ -102,6 +102,7 @@ export const TOOLS: ToolCategory[] = [
 	  { name: 'Gemini', url: 'https://gemini.google.com/app', iconName: 'Bot' },
 	  { name: 'ChatGPT', url: 'https://chatgpt.com/', iconName: 'Bot' },
 	  { name: 'AI studio', url: 'https://aistudio.google.com/', iconName: 'MonitorCloud' },
+	  { name: 'NotebookLM', url: 'https://notebooklm.google.com/', iconName: 'NotebookPen' },
 	  { name: 'Flow', url: 'https://labs.google/fx/zh/tools/flow', iconName: 'Clapperboard' }
     ]
   }
